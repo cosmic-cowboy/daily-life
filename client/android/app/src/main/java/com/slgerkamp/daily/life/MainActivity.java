9b693fd4e6e842564ec9a592cac9b08d0ccee50f
hiroo takaku <slgerkamp@gmail.com>
2015-08-10 19:57:40 +0900

    ツールバー対応

--- client/android/app/src/main/java/com/slgerkamp/daily/life/MainActivity.java
+++ client/android/app/src/main/java/com/slgerkamp/daily/life/MainActivity.java
@@ -2,16 +2,27 @@ package com.slgerkamp.daily.life;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.main_page);
+
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
