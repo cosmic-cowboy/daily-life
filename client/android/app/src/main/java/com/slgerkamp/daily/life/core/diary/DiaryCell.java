a7a96ef4fc3b29d3c38f84d7ac05126a9dee2554
hiroo takaku <slgerkamp@gmail.com>
2015-08-31 07:14:46 +0900

    トーク一覧のレイアウト修正

--- client/android/app/src/main/java/com/slgerkamp/daily/life/core/diary/DiaryCell.java
+++ client/android/app/src/main/java/com/slgerkamp/daily/life/core/diary/DiaryCell.java
@@ -1,7 +1,9 @@
 package com.slgerkamp.daily.life.core.diary;
 
+import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
 import com.slgerkamp.daily.life.R;
@@ -24,6 +26,38 @@ public class DiaryCell {
     public void setItem(DiaryItem item) {
         post_date.setText(DateFormat.format("M/d", item.postDate).toString());
         content.setText(item.content);
+        ellipsizeMultilineText(content, 2);
     }
 
+    /**
+     *
+     */
+    public static void ellipsizeMultilineText(final TextView textView, final int maxLines) {
+        textView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                textView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                doEllipsizeMultilineText(textView, maxLines);
+            }
+        });
+    }
+
+    private static void doEllipsizeMultilineText(TextView view, int maxLines) {
+        if (maxLines >= view.getLineCount()) return;
+
+        // 行の長さをピクセル数で計算?
+        float avail = 0.0f;
+        for (int i = 0; i < maxLines; i++) {
+            avail += view.getLayout().getLineMax(i);
+        }
+
+        // テキストを更新
+        CharSequence ellipsizedText = TextUtils.ellipsize(
+                view.getText(),
+                view.getPaint(),
+                avail,
+                TextUtils.TruncateAt.END
+        );
+        view.setText(ellipsizedText);
+    }
 }
