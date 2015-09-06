2a742d79aca303b6e539090f69c404d1891a4458
hiroo takaku <slgerkamp@gmail.com>
2015-09-06 22:03:30 +0900

    ドメインID生成
    ドメインごとに利用するlong型のIDの基幹クラスを作成
    ファイルを表すIDを作成

---
+++ server/src/main/java/com/slgerkamp/daily/life/infra/fileio/FileId.java
@@ -0,0 +1,15 @@
+package com.slgerkamp.daily.life.infra.fileio;
+
+import com.slgerkamp.daily.life.infra.modeling.LongValueId;
+
+/**
+ * <p>ファイルを表すIDです。
+ *
+ */
+public class FileId extends LongValueId{
+
+	public FileId(long value) {
+		super(value);
+	}
+
+}
---
+++ server/src/main/java/com/slgerkamp/daily/life/infra/modeling/LongValueId.java
@@ -0,0 +1,42 @@
+package com.slgerkamp.daily.life.infra.modeling;
+
+/**
+ * <p>long型でIDで表すときに利用する基幹クラスです。
+ *
+ */
+public abstract class LongValueId {
+
+	private final long value;
+	
+	/**
+	 * <p>コンストラクタ
+	 * @param value
+	 */
+	protected LongValueId(long value) {
+		this.value = value;
+	}
+	
+	public final long longValue(){
+		return value;
+	}
+
+	// ----------------------------------------------------------------
+	//    おまじない
+	// ----------------------------------------------------------------
+	@Override
+	public int hashCode() {
+		int result = 1;
+		result = 31 * result + (int) (value ^ (value >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		LongValueId other = (LongValueId) obj;
+		if (value != other.value) return false;
+		return true;
+	}
+}
