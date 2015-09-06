341041608aaafdde9284a77c43765baa5f8ffa25
hiroo takaku <slgerkamp@gmail.com>
2015-09-06 22:07:15 +0900

    ファイルストレージ機能
    ローカルストレージにファイルを保存する機能を実装

---
+++ server/src/main/java/com/slgerkamp/daily/life/infra/fileio/storage/FileStorage.java
@@ -0,0 +1,40 @@
+package com.slgerkamp.daily.life.infra.fileio.storage;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public interface FileStorage {
+
+	/**
+	 * <p>ファイルを書き込みます。
+	 * @param input
+	 * @param path
+	 */
+	void write(InputStream input, String path) throws StorageException;
+	
+	/**
+	 * <p>ファイルを読み込みます。
+	 * @param path
+	 * @return
+	 */
+	InputStream read(String path) throws FileNotFoundException;
+	
+	/**
+	 * <p>ファイルを削除します。
+	 * @param path
+	 */
+	void delete(String path) throws StorageException;
+	
+	/**
+	 * <p>ストレージで発生した例外を表します。
+	 */
+	public class StorageException extends Exception {
+
+		private static final long serialVersionUID = 1L;
+
+		public StorageException(String operation, String path, Throwable cause) {
+			super("ストレージで例外が発生しました：" + operation + " " + path, cause);
+		}
+	}
+
+}
