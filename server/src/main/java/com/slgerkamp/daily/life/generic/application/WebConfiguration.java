0ca3f4326040f2017e3c6574d61a9b1d1541fc29
hiroo takaku <slgerkamp@gmail.com>
2015-09-26 17:18:34 +0900

    h2 database の 内容を確認するためのWebConsoleの実装

---
+++ server/src/main/java/com/slgerkamp/daily/life/generic/application/WebConfiguration.java
@@ -0,0 +1,20 @@
+package com.slgerkamp.daily.life.generic.application;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p> h2 database の 内容を確認するためのWebConsoleです。
+ *
+ */
+@Configuration
+public class WebConfiguration {
+	@Bean
+	ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
+	}
+}
