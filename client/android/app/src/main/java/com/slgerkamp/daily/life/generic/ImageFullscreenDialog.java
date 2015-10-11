c2bae7b51471dd1bec9314513819b42bf3285ee5
hiroo takaku <slgerkamp@gmail.com>
2015-10-11 12:24:16 +0900

    画像を拡大表示するダイアログを追加

---
+++ client/android/app/src/main/java/com/slgerkamp/daily/life/generic/ImageFullscreenDialog.java
@@ -0,0 +1,71 @@
+package com.slgerkamp.daily.life.generic;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.slgerkamp.daily.life.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * <p>画像を全画面で表示するためのダイアログ</p>
+ */
+public class ImageFullscreenDialog extends DialogFragment{
+
+    private static final String PARAM_FILE_ID = "fileId";
+
+    @InjectView(R.id.image_view) ImageView imageView;
+    @InjectView(R.id.cancel) ImageButton cancel;
+
+    public static ImageFullscreenDialog newInstance(Long fileId) {
+
+        ImageFullscreenDialog fragment = new ImageFullscreenDialog();
+        Bundle args = new Bundle();
+        args.putLong(PARAM_FILE_ID, fileId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ImageFullscreenDialog() {}
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        final Long fileId = getArguments().getLong(PARAM_FILE_ID);
+
+        LayoutInflater layoutInflater = getActivity().getLayoutInflater();
+        View root = layoutInflater.inflate(R.layout.image_fullscreen_dialog, null);
+        ButterKnife.inject(this, root);
+
+        new Backend(getActivity()).imageLoader().load(fileId).into(imageView);
+
+        // フルスクリーンで表示するため、Dialogのレイアウト情報を変更
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(root);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.BLACK));
+        dialog.getWindow().setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+
+        return dialog;
+    }
+}
